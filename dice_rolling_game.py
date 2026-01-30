a13d737713a4f52d05d055c1ee34becd73ba1048
th3mon <p.szelenberger@gmail.com>
2026-01-30 16:31:49 +0100

    Roll a variable number of dice

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -2,9 +2,24 @@ import random
 
 messages = {
     "question": "Roll the dice? (y/n): ",
+    "dice_quantity": "How many dice you want to roll? (number): ",
     "invalid": "Invalid choice!",
     "thanks": "Thanks for playing!",
 }
+
+
+def roll_dice(dice_quantity: str):
+    messages = {"error": "Quantity should be a number"}
+    try:
+        dice_quantity_as_number = int(dice_quantity)
+        results = {"values": []}
+
+        for _ in range(dice_quantity_as_number):
+            results.get("values").append(str(get_d6_result()))
+
+        return results
+    except Exception:
+        return {"error": messages.get("error")}
 
 
 def get_d6_result():
@@ -15,10 +30,14 @@ while True:
     choice = input(messages.get("question")).lower()
 
     if choice == "y":
-        dice1 = get_d6_result()
-        dice2 = get_d6_result()
+        dice_quantity = input(messages.get("dice_quantity"))
+        results = roll_dice(dice_quantity)
 
-        print(f"({dice1} {dice2})")
+        if results.get("error"):
+            print(results.get("error"))
+        else:
+            dice = ", ".join(results.get("values"))
+            print(f"({dice})")
 
     elif choice == "n":
         print(messages.get("thanks"))
